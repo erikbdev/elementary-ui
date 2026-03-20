7b1083a0d44398d5d84f263783ce1f7f740ea507
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2026-03-20 16:10:13 +0100

    bump elementary version (#89)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swiftwasm/JavaScriptKit", .upToNextMinor(from: "0.46.3")),
-        .package(url: "https://github.com/elementary-swift/elementary", from: "0.6.3"),
+        .package(url: "https://github.com/elementary-swift/elementary", from: "0.7.0"),
         .package(url: "https://github.com/swiftlang/swift-syntax", "600.0.0"..<"603.0.0"),
     ],
     targets: [
